455edb2d3edac18538d6224e5b7720148ede1a7a
Mick Vleeshouwer <mick@imick.nl>
2024-09-30 22:00:56 +0000

    Improve audio save logic

--- server/python/app/audio.py
+++ server/python/app/audio.py
@@ -3,6 +3,8 @@
 import numpy as np
 import wave
 
+from .enums import MediaFormat
+
 
 def ulaw2linear(ulaw):
     """Convert u-Law encoded bytes to Linear-16 PCM values"""
@@ -15,11 +17,18 @@ def ulaw2linear(ulaw):
     linear = (mantissa << 4) + 0x08
     linear = linear << exponent
     linear = np.where(sign != 0, linear - 0x84, 0x84 - linear)
+
     return linear
 
 
-def save_to_wav(filename, audio_data, channels, sample_width, frame_rate):
+def save_to_wav(filename, format, audio_data, channels, sample_width, frame_rate):
     """Save audio data to a WAV file."""
+
+    # Convert the linear PCMU data to bytes
+    if format == MediaFormat.PCMU:
+        linear_pcm = ulaw2linear(audio_data)
+        audio_data = linear_pcm.tobytes()
+
     with wave.open(filename, "wb") as wav_file:
         wav_file.setnchannels(channels)
         wav_file.setsampwidth(sample_width)
