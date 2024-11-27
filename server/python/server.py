c992dd51e5885e5016a1fa8fa78aebd9d9ec740d
Mick Vleeshouwer <mick@imick.nl>
2024-11-27 21:06:18 +0000

    Bugfix

--- server/python/server.py
+++ server/python/server.py
@@ -4,6 +4,8 @@ import os
 from app.websocket_server import WebsocketServer
 from dotenv import find_dotenv, load_dotenv
 
+load_dotenv(find_dotenv())
+
 LOGGER: logging.Logger = logging.getLogger(__name__)
 
 # Set logging level based on environment variables
@@ -16,7 +18,6 @@ else:
 # Run development server when running this script directly.
 # For production it is recommended that Quart will be run using Hypercorn or an alternative ASGI server.
 if __name__ == "__main__":
-    load_dotenv(find_dotenv())
     server = WebsocketServer()
     server.app.run()
 else:
