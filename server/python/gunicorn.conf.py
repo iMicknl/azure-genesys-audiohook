26970f9991cb2034dba1f837a5ce73ce3208975f
Mick Vleeshouwer <mick@imick.nl>
2025-03-28 22:05:50 +0000

    Temp only use 1 worker (no horizontal scale)

--- server/python/gunicorn.conf.py
+++ server/python/gunicorn.conf.py
@@ -9,5 +9,7 @@ timeout = 230
 # https://learn.microsoft.com/en-us/troubleshoot/azure/app-service/web-apps-performance-faqs#why-does-my-request-time-out-after-230-seconds
 
 num_cpus = multiprocessing.cpu_count()
-workers = (num_cpus * 2) + 1
+# workers = (num_cpus * 2) + 1
 worker_class = "uvicorn.workers.UvicornWorker"
+
+workers = 1
